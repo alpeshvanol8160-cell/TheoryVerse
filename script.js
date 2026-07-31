24d0f2b484c68885cc449e0e811965064e846884
alpeshvanol8160-cell <alpeshvanol8160@gmail.com>
2026-07-31 20:15:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -64,29 +64,3 @@ navLinks.forEach(link => {
         link.classList.add("active");
     }
 });
-// Dark Mode
-function toggleTheme() {
-    document.body.classList.toggle("dark-mode");
-}
-
-// Scroll to Top
-window.onscroll = function () {
-    let btn = document.getElementById("topBtn");
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-        btn.style.display = "block";
-    } else {
-        btn.style.display = "none";
-    }
-};
-
-function topFunction() {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    });
-}
-
-// Loader
-window.onload = function () {
-    document.getElementById("loader").style.display = "none";
-};
