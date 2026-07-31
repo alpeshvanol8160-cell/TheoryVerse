9a2658dc1b8930654c7a45cd10fe44449e56db47
alpeshvanol8160-cell <alpeshvanol8160@gmail.com>
2026-07-31 15:38:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ function searchTheory() {
         alert("Theory not found!");
     }
 }
+let topBtn = document.getElementById("topBtn");
+
+window.onscroll = function () {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+};
+
+function topFunction() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
